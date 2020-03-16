0168137aa68a6ec77be19d56cb5749f65e73485d
daiki <daiking_tensai@yahoo.co.jp>
2020-03-16 17:31:50 +0900

    add usb-serial lib

--- app/src/main/java/com/example/homegateway/MainActivity.java
+++ app/src/main/java/com/example/homegateway/MainActivity.java
@@ -1,13 +1,90 @@
 package com.example.homegateway;
 
+import android.hardware.usb.UsbDevice;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import android.content.Context;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import android.app.Activity;
+import android.content.Context;
+import android.hardware.usb.UsbManager;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.ftdi.j2xx.D2xxManager;
+import com.ftdi.j2xx.FT_Device;
+
 public class MainActivity extends AppCompatActivity {
+
+    private FT_Device ftDev = null;
+    private static Context mContext;
+    private D2xxManager ftdid2xx;
+    private static final String TAG = "USB";
+    private int iavailable = 0;
+    private static final int readLength = 512;
+    byte[] readData;
+    char[] readDataToText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        try {
+            ftdid2xx = D2xxManager.getInstance(this);
+        } catch (D2xxManager.D2xxException ex) {
+            Log.e(TAG,ex.toString());
+        }
+
+        mContext = this.getBaseContext();
+
+        openUsb();
+
     }
+
+    public void openUsb(){
+        int devCount = 0;
+        devCount = ftdid2xx.createDeviceInfoList(this);
+        if (devCount <= 0)
+        {
+            Toast.makeText(this, "デバイスが発見できません。" + Integer.toString(devCount), Toast.LENGTH_LONG).show();
+            Log.i(TAG,"デバイスが発見できましせんでした。");
+            return;
+        }
+        else{
+            Toast.makeText(this, "" + devCount + "個のデバイスを発見しました。", Toast.LENGTH_LONG).show();
+            Log.i(TAG,"" + devCount + "個のデバイスを発見しました。");
+        }
+
+        if(null == ftDev)
+        {
+            ftDev = ftdid2xx.openByIndex(mContext, 0);
+        }
+        else
+        {
+            synchronized(ftDev)
+            {
+                ftDev = ftdid2xx.openByIndex(mContext, 0);
+            }
+        }
+
+        //ftDev.setBitMode((byte) 0, D2xxManager.FT_BITMODE_RESET);
+        ftDev.setBaudRate(9600);
+        ftDev.setDataCharacteristics(D2xxManager.FT_DATA_BITS_8, D2xxManager.FT_STOP_BITS_1, D2xxManager.FT_PARITY_NONE);
+        ftDev.setFlowControl(D2xxManager.FT_FLOW_NONE, (byte) 0x0b, (byte) 0x0d);
+        ftDev.purge((byte) (D2xxManager.FT_PURGE_TX | D2xxManager.FT_PURGE_RX));
+        ftDev.restartInTask();
+
+    }
+
 }
